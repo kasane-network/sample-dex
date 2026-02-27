0d6254c1d517c987d55a7171557c55642942d5f2
hude <qwertyuiop123456@gmail.com>
2026-02-27 09:07:13 +0900

    chore: remove non-web i18n/extension assets and keep en-US only

--- v2-interface/packages/uniswap/src/utils/usePlatformBasedValue.ts
+++ v2-interface/packages/uniswap/src/utils/usePlatformBasedValue.ts
@@ -1,7 +1,3 @@
-import { useIsChromeWindowFocusedWithTimeout } from 'uniswap/src/extension/useIsChromeWindowFocused'
-import { isExtensionApp } from 'utilities/src/platform'
-import { ONE_SECOND_MS } from 'utilities/src/time/time'
-
 export type UsePlatformBasedValue<T> = {
   defaultValue: T
   mobile?: {
@@ -17,17 +13,6 @@ export type UsePlatformBasedValue<T> = {
 }
 
 export function usePlatformBasedValue<T>({ defaultValue, web, extension }: UsePlatformBasedValue<T>): T {
-  // We add a 30s delay before we trigger the `windowNotFocused` state to avoid unnecessary state changes when the user is quickly switching back-and-forth between windows.
-  // Without this delay, we could end up triggering too many unnecessary API calls every time the window regains focus.
-  const isChromeWindowFocused = useIsChromeWindowFocusedWithTimeout(30 * ONE_SECOND_MS)
-
-  if (isExtensionApp) {
-    if (!isChromeWindowFocused) {
-      return extension?.windowNotFocused ?? defaultValue
-    }
-
-    return extension?.defaultValue ?? defaultValue
-  }
-
+  void extension
   return web?.defaultValue ?? defaultValue
 }
